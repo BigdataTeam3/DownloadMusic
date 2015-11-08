7f715a734c594cb38c6d076efc554b659d2dc374
Ray <ray7476@gmail.com>
2015-11-08 14:52:18 +0800

    mode for melody

---
+++ model/mainfun/Rmainfunc.R
@@ -0,0 +1,33 @@
+
+ ## random forest (train data)
+data = read.csv("E:\\DownloadMusicR\\Musical Analysis\\traindata.csv",header = FALSE)
+library(caTools)
+spl = sample.split(data$V5, SplitRatio = 1)
+ClaimsTrain = subset(data, spl==TRUE)
+table(ClaimsTrain$V5)
+ClaimsTest = subset(data, spl==FALSE)
+y = glm(V5~V1+V2+V3,data = data) 
+#V5~V1+V2+V3 : V5 = aV1+bV2+cV3 ；v5:應變數、V1~V3:自變數
+PredictTest = predict(y, newdata = ClaimsTest, type = "response")
+table(ClaimsTest$V5, PredictTest>0.7)
+
+#library(ROCR)
+#pred = prediction(PredictTest,ClaimsTest$V5)
+#perf = performance(pred,"tpr","fpr")
+#plot(perf)
+#as.numeric(performance(pred,"auc")@y.values)
+
+
+## random forest_real 
+ClaimsTest = read.csv("E:\\DownloadMusicR\\Musical Analysis\\mainfun.csv",header = FALSE)
+PredictTest = predict(y, newdata = ClaimsTest, type = "response")
+library(car)
+if(any(PredictTest >0.7)){
+  ifelse (PV <- PredictTest>0.7,1,0)
+  }else{PV <- recode(PredictTest,"max(PredictTest)=1;else =0")}
+
+value = list('staff'=ClaimsTest$V5,'mainMelody'=PV)
+RR <- as.data.frame(value)
+
+write.csv(RR, file = "MyData.csv",row.names=FALSE)
+

f72e784c90ec4c080ba2e57eb824ebbe551e9801
henry <henrypsn@gmail.com>
2015-12-01 12:10:49 +0800

    revise by Henry 2015/12/01

--- chording/N_gram.py
+++ chording/N_gram.py
@@ -1,18 +1,19 @@
-# coding=big5
+# coding=utf-8
 from operator import itemgetter
 import csv
 from math import log
 
-def N_gram_main_function(Dominantlist,N_gram_number):
-	if N_gram_number == 4:
-		Dominantlist_4_gram_number(Dominantlist,N_gram_number)
-	elif N_gram_number == 8:
-		Dominantlist_8_gram_number(Dominantlist,N_gram_number)
-	elif N_gram_number == 16:
-		Dominantlist_16_gram_number(Dominantlist,N_gram_number)
+def N_gram_main_function(Dominantlist):
+	N_gram_number = raw_input("請輸入N-gram-number，4 or 8 or 16".decode('cp950'))
+	if int(N_gram_number) == 4:
+		Dominantlist_4_gram_number(Dominantlist,int(N_gram_number))
+	elif int(N_gram_number) == 8:
+		Dominantlist_8_gram_number(Dominantlist,int(N_gram_number))
+	elif int(N_gram_number) == 16:
+		Dominantlist_16_gram_number(Dominantlist,int(N_gram_number))
 	else :
-		print u'請重新輸入N-gram-number，4 or 8 or 16'
-
+		print '請重新輸入N-gram-number，4 or 8 or 16'.decode('cp950')
+		N_gram_main_function(Dominantlist)
 
 #一次抓4個
 	
@@ -104,9 +105,9 @@ def Dominantsorted_possible_transfer_clean(Dominantsorted):
 #找出和弦組合
 def Dominantclean_to_Dominantcompare(Compare_list,Dominantsorted):
 	Compare_Dominant = [Dominantsorted[i] for i in range(len(Dominantsorted)) for j in Compare_list if Dominantsorted[i][0] == j]
-	print u'可用來比對的和弦組合'
+	print '可用來比對的和弦組合'.decode('cp950')
 	print Compare_Dominant
-	print u'總共有幾個和弦組合可用來比對 =',len(Compare_list)
+	print '總共有幾個和弦組合可用來比對 ='.decode('cp950'),len(Compare_list)
 	Compare_number = Compare_number_func(len(Compare_list))
 	return Compare_Dominant,Compare_number
 	
@@ -115,12 +116,12 @@ def Compare_number_func(Compare_length):
     Compare_length_list = []
     Compare_length_list = [i for i in range(Compare_length)]
     Compare_number = 0
-    Compare_number = raw_input(u"請輸入number，範圍從{}到{}".format(Compare_length_list[0],Compare_length_list[-1]))
+    Compare_number = raw_input("請輸入number，範圍從{}到{}".decode('cp950').format(Compare_length_list[0],Compare_length_list[-1]))
     if Compare_number.isdigit():
         if (int(Compare_number) in Compare_length_list):
             return int(Compare_number)
         elif (int(Compare_number) not in Compare_length_list):
-            print u'請重新輸入number'
+            print '請重新輸入number'.decode('cp950')
             return Compare_number_func(Compare_length)
     else :
         return Compare_number_func(Compare_length)
@@ -139,9 +140,9 @@ def Dominantsorted_count_sum(x,y):
 #目前一次十六個和弦，最多錯三個和弦，然後去掉重複的，做成list，用來分類
 def Compare_Dominant_classify(Compare_Dominant,Compare_number,N_gram_number):
 	Compare_list2 = []
-	print u'用來比對的和弦'
+	print '用來比對的和弦'.decode('cp950')
 	print Compare_Dominant[Compare_number][0]
-	print u'和弦組合當中，類似的組合'
+	print '和弦組合當中，類似的組合'.decode('cp950')
 	
 	missnumber = int(log(N_gram_number,2))-1
 
@@ -151,9 +152,9 @@ def Compare_Dominant_classify(Compare_Dominant,Compare_number,N_gram_number):
 			if cmp(Compare_Dominant[Compare_number][0][j],Compare_Dominant[i][0][j]) == 0 : Compare_count += 1       
 		print Compare_count,Compare_Dominant[i][0]
 		if Compare_count >= (len(Compare_Dominant[0][0])-missnumber) : Compare_list2.append(Compare_Dominant[i])
-	print u'挑選的結果'
+	print '挑選的結果'.decode('cp950')
 	print Compare_list2
-	print u'歸類到最多次數的和弦'
+	print '歸類到最多次數的和弦'.decode('cp950')
 	Compare_list3 = max(Compare_list2,key = Dominantsorted_count_comparator)
 	Compare_list4 = reduce(Dominantsorted_count_sum,Compare_list2,[Compare_list3[0],0])
 	print Compare_list4

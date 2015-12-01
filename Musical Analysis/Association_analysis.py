138b354cf939e91bce06b24c84d12979094e84e3
henry <henrypsn@gmail.com>
2015-12-01 19:33:29 +0800

    revise by Henry 2015/12/01

--- Musical Analysis/Association_analysis.py
+++ Musical Analysis/Association_analysis.py
@@ -19,7 +19,9 @@ def Association_analysis_mainfunction(dicts,percent_value):
 # print Measure_dict
 	Transfer_dic = Transfer_dictdata(dicts,Measure_dict)
 	print Transfer_dic
-	
+
+# 先將dicts of dicts 的value取出來，以一個Measure為單位，建立lists，
+# 如果有多個track在同一個Measure，合併到同一個lists裡面
 def Measure_extract_from_dictdata(dicts):
     values_lists = [i.values() for i in dicts.values()]
     values_lists = [[','.join(values_lists[i])] for i in range(len(values_lists))]
@@ -32,7 +34,8 @@ def Transfer_type_int_or_float(Measure_element):
         return int(Measure_element)
     else:
         return float(Measure_element)
-        
+
+# 累計每個Measure lists出現的次數，並建立 排序dicts，{key = measure_lists , value = 次數}，並且依次數排序
 def Establish_percussion_dict_sorted(values_lists):
     Percussion_dict = dict()
     for measure in values_lists:
@@ -45,14 +48,16 @@ def Count_total(x, y):
 	
 def Transfer_percent(x, y):
     return round(float(x)/float(y),3)
-	
+
+# 從 排序dicts，再建立一個  百分比轉換dicts，{key = 次數，value = 轉成百分比}
 def Establish_percent_dict(Percussion_dict):
     count_list = [i[1] for i in Percussion_dict]
     count_totals = 0 
     count_totals = reduce(Count_total,count_list)
     percent_dict = {i:Transfer_percent(i,count_totals) for i in count_list}
     return percent_dict
-    
+   
+# 將 排序dicts，套用  百分比轉換dicts
 def Transfer_percussion_dict_percent(Percussion_dict,percent_dict):
     Transfer_percussion_dict = dict()
     for (key,value) in Percussion_dict:
@@ -60,6 +65,8 @@ def Transfer_percussion_dict_percent(Percussion_dict,percent_dict):
     Transfer_percussion_dict = sorted(Transfer_percussion_dict.items(), key=itemgetter(1), reverse=True)
     return Transfer_percussion_dict
 
+# 對轉換完的 排序dicts，根據百分比，再建立 標籤dicts，
+#{Measure lists，轉成標籤，大於percent_value，為A1,A2...，小於percent_value，為B1,B2...}
 def Establish_measure_dict(Transfer_percussion_dict,percent_value):
     measure_dict = dict()
     count_A = 0
@@ -73,6 +80,8 @@ def Establish_measure_dict(Transfer_percussion_dict,percent_value):
             measure_dict.update({key : 'B'+str(count_B)})
     return measure_dict
 
+# 套用標籤dicts
+# 轉換原先的dicts，全部Measure 轉成標籤
 def Transfer_dictdata(dicts,measure_dict):
     for keys,values in dicts.items():
         for key,value in values.items():

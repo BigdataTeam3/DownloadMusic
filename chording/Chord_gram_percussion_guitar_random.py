9e1200befa4fd0ce7f4c571a882244ee81160c85
henry <henrypsn@gmail.com>
2015-12-12 22:54:42 +0800

    debug and add function by Henry 2015/12/12

--- chording/Chord_gram_percussion_guitar_random.py
+++ chording/Chord_gram_percussion_guitar_random.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 
-#直接使用 random_main()
+#直接使用 random_main_with_instrument()
 
 from pymongo import MongoClient
 import pymongo,random
@@ -20,59 +20,103 @@ guitar_dict = {
     '84':'EG_synth',
 }
 
-#產出結果，gram_key字串，8g 及 16g 固定，後面的數字random，
+gram_list = [4,8,16,32]
+
+#產出結果，gram_key字串，4g 及 8g 及 16g 及 32g 固定，後面的數字random，
 #但不會超出該collection的document數量
 
-def N_gram_random_with_N(get_key):
+def N_gram_random_with_N(get_gram_key):
 	collect = db['clearedGramPattern']  #選擇database.collection
-	N = int(get_key)
+	N = int(get_gram_key)
 	
-	if N == 8:
+	if N == 4:
+		cursor = collect.find({'4gram':{'$exists':True}})
+		chord_gram_count = cursor.count()
+		chord_gram_random = random.randint(1,chord_gram_count)
+		
+		gram_string = '4g'
+		gram_key = gram_string + str(chord_gram_random)
+		return gram_key
+	
+	elif N == 8:
 		cursor = collect.find({'8gram':{'$exists':True}})
-		chord_8gram_count = cursor.count()
-		chord_8gram_random = random.randint(1,chord_8gram_count)
+		chord_gram_count = cursor.count()
+		chord_gram_random = random.randint(1,chord_gram_count)
 		
 		gram_string = '8g'
-		gram_key = gram_string + str(chord_8gram_random)
+		gram_key = gram_string + str(chord_gram_random)
 		return gram_key
 		
 	elif N == 16:
 		cursor = collect.find({'16gram':{'$exists':True}})
-		chord_16gram_count = cursor.count()
-		chord_16gram_random = random.randint(1,chord_16gram_count)
+		chord_gram_count = cursor.count()
+		chord_gram_random = random.randint(1,chord_gram_count)
 		
 		gram_string = '16g'
-		gram_key = gram_string+str(chord_16gram_random)
+		gram_key = gram_string+str(chord_gram_random)
 		return gram_key
+	
+	elif N == 32:
+		cursor = collect.find({'32gram':{'$exists':True}})
+		chord_gram_count = cursor.count()
+		chord_gram_random = random.randint(1,chord_gram_count)
 		
+		gram_string = '32g'
+		gram_key = gram_string+str(chord_gram_random)
+		return gram_key
+
+# 完全random時，使用N_gram_random_without_N
+# 完全random時，4g及32g，跟柏安討論後，待議
+
 def N_gram_random_without_N():
 	collect = db['clearedGramPattern']  #選擇database.collection
 	
-	gram_string = random.choice (['8g', '16g'])
+	gram_string = random.choice(['8g','16g'])
+	
+	# gram_string = random.choice(['4g','8g','16g','32g'])
 	
 	if gram_string == '8g':
 		cursor = collect.find({'8gram':{'$exists':True}})
-		chord_8gram_count = cursor.count()
-		chord_8gram_random = random.randint(1,chord_8gram_count)
+		chord_gram_count = cursor.count()
+		chord_gram_random = random.randint(1,chord_gram_count)
 		
-		gram_key = gram_string + str(chord_8gram_random)
-		return gram_key
+		gram_key = gram_string + str(chord_gram_random)
+		get_gram_key = gram_string.split('g')[0]
+		return gram_key,get_gram_key
 		
 	elif gram_string == '16g':
 		cursor = collect.find({'16gram':{'$exists':True}})
-		chord_16gram_count = cursor.count()
-		chord_16gram_random = random.randint(1,chord_16gram_count)
+		chord_gram_count = cursor.count()
+		chord_gram_random = random.randint(1,chord_gram_count)
 		
-		gram_key = gram_string+str(chord_16gram_random)
-		return gram_key
+		gram_key = gram_string + str(chord_gram_random)
+		get_gram_key = gram_string.split('g')[0]
+		return gram_key,get_gram_key
 	
-
-#產出結果，percussion_tuple，tuple包含字串，依據get_key 決定tuple的長度，
+	# elif gram_string == '32g':
+		# cursor = collect.find({'32gram':{'$exists':True}})
+		# chord_gram_count = cursor.count()
+		# chord_gram_random = random.randint(1,chord_gram_count)
+		
+		# gram_key = gram_string + str(chord_gram_random)
+		# get_gram_key = gram_string.split('g')[0]
+		# return gram_key,get_gram_key
+	
+	# elif gram_string == '4g':
+		# cursor = collect.find({'4gram':{'$exists':True}})
+		# chord_gram_count = cursor.count()
+		# chord_gram_random = random.randint(1,chord_gram_count)
+		
+		# gram_key = gram_string + str(chord_gram_random)
+		# get_gram_key = gram_string.split('g')[0]
+		# return gram_key,get_gram_key
+	
+#產出結果，percussion_tuple，tuple包含字串，依據grams_key 決定tuple的長度，
 # A 及 B 固定，後面的數字random，但不會超出該collection的document數量
 
-def percussion_random_with_N(get_key):
+def percussion_random_with_N(get_gram_key):
 	
-	N = int(get_key)
+	N = int(get_gram_key)
 	
 	collect = db['percussion_pattern_with_track_A_pattern']  #選擇database.collection
 	cursor = collect.find({})
@@ -92,12 +136,30 @@ def percussion_random_with_N(get_key):
 
 	
 # 產出結果，collection名字 及 guitar_A_pattern_tuple
-# tuple包含字串，依據get_key 決定tuple的長度，
+# tuple包含字串，依據grams_key 決定tuple的長度，
 # A 固定，後面的數字random，但不會超出該collection的document數量
 
-def guitar_random_with_N(get_key):
+def guitar_random_with_N(get_gram_key,guitar_key):
 
-	N = int(get_key)
+	N = int(get_gram_key)
+	guitar_key_str = str(guitar_key)
+	
+	guitar_string = guitar_dict[guitar_key_str]
+	collect = db[guitar_string]  #選擇database.collection
+	
+	cursor = collect.find({'pattern':'A'}) # .sort("_id", pymongo.ASCENDING)
+	guitar_A_pattern_count = cursor.count()
+	guitar_A_pattern_random = random.randint(1,guitar_A_pattern_count)
+	
+	guitar_A_pattern_string = "A" + str(guitar_A_pattern_random)
+	guitar_A_pattern_tuple = tuple([guitar_A_pattern_string]*(N/2))
+	
+	return guitar_string,guitar_A_pattern_tuple
+
+# 完全random時，使用guitar_random_without_N
+def guitar_random_without_N(get_gram_key):
+
+	N = int(get_gram_key)
 	
 	guitar_string = random.choice([guitar_dict[key] for key in guitar_dict])
 	collect = db[guitar_string]  #選擇database.collection
@@ -113,16 +175,49 @@ def guitar_random_with_N(get_key):
 
 	
 #產出結果，random_dict，{key = gram : value = {percussion_pattern + guitar_pattern}}
-
-def random_main(get_key):
+def random_main_with_instrument(get_gram_key,guitar_key):
 
 	random_dict = dict()
 	
-	gram_key = N_gram_random_with_N(get_key)
-	percussion_tuple = percussion_random_with_N(get_key)
-	guitar_string,guitar_A_pattern_tuple = guitar_random_with_N(get_key)
+	gram_key = N_gram_random_with_N(get_gram_key)
+	percussion_tuple = percussion_random_with_N(get_gram_key)
+	guitar_string,guitar_A_pattern_tuple = guitar_random_with_N(get_gram_key,guitar_key)
+	
+	random_dict.update({gram_key:{'percussion':percussion_tuple,guitar_string:guitar_A_pattern_tuple}})
+	
+	return random_dict
+
+# 完全random時，使用random_main_without_instrument，可當作預設值
+def random_main_without_instrument():
+
+	random_dict = dict()
+	
+	gram_key,get_gram_key = N_gram_random_without_N()
+	percussion_tuple = percussion_random_with_N(get_gram_key)
+	guitar_string,guitar_A_pattern_tuple = guitar_random_without_N(get_gram_key)
 	
 	random_dict.update({gram_key:{'percussion':percussion_tuple,guitar_string:guitar_A_pattern_tuple}})
 	
 	return random_dict
 	
+	
+def random_main(times,*args):
+
+	random_list_final = list()
+	
+	if args:
+		args_list = [arg for arg in args]
+		if times and (int(args_list[0]) in gram_list) and (str(args_list[1]) in guitar_dict.keys()):
+			get_gram_key = args_list[0];
+			guitar_key = args_list[1];
+			random_list_final = [[random_main_with_instrument(get_gram_key,guitar_key)]*2 for i in range(times)]
+			random_list_final = [j for i in random_list_final for j in i]
+			return random_list_final
+
+	elif not args:
+		random_list_final = [[random_main_without_instrument()]*2 for i in range(times)]
+		random_list_final = [j for i in random_list_final for j in i]
+		return random_list_final
+	
+	else:
+		pass

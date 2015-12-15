478d33dfb4ff642e1e9faa6bbd3efdae977851fc
henry <henrypsn@gmail.com>
2015-12-15 16:39:00 +0800

    revise by Henry 2015/12/15

--- chording/N_gram.py
+++ chording/N_gram.py
@@ -5,111 +5,50 @@ import csv,sys,pymongo
 from math import log
 from pymongo import MongoClient
 
+chord_list = [4,8,16,32]
 
-def N_gram_main_function_single(Dominantlist):
-	N_gram_number = raw_input("請輸入N-gram-number，4 or 8 or 16 or 32".decode('cp950'))
-	if int(N_gram_number) == 4:
-		Dominantlist_4_gram_number(Dominantlist,int(N_gram_number))
-	elif int(N_gram_number) == 8:
-		Dominantlist_8_gram_number(Dominantlist,int(N_gram_number))
-	elif int(N_gram_number) == 16:
-		Dominantlist_16_gram_number(Dominantlist,int(N_gram_number))
-	elif int(N_gram_number) == 32:
-		Dominantlist_16_gram_number(Dominantlist,int(N_gram_number))
-	else :
-		print "請重新輸入N-gram-number，4 or 8 or 16 or 32".decode('cp950')
-		N_gram_main_function(Dominantlist)
+# def N_gram_main_function_single(chordlist):
+	# N_gram_number = raw_input("請輸入N-gram-number，{}".decode('cp950').format(chord_list))
+	
+	# if int(N_gram_number) in chord_list:
+		# chord_gram(chordlist,N_gram_number,Compare_number)
 
-def N_gram_main_function_multiple(Dominantlist,N_gram_number,Compare_number):
-	if N_gram_number == 4:
-		Dominantlist_4_gram_number(Dominantlist,N_gram_number,Compare_number)
-	elif N_gram_number == 8:
-		Dominantlist_8_gram_number(Dominantlist,N_gram_number,Compare_number)
-	elif N_gram_number == 16:
-		Dominantlist_16_gram_number(Dominantlist,N_gram_number,Compare_number)
-	elif N_gram_number == 32:
-		Dominantlist_16_gram_number(Dominantlist,N_gram_number,Compare_number)
+	# else :
+		# print "請重新選擇chord_pattern，{}".decode('cp950').format(chord_list)
+		# N_gram_main_function_single(chordlist)
+
+def N_gram_main_function_multiple(chordlist,**keyword):
+	N_gram_number = keyword['N_gram_number']
+	Compare_number = keyword['Compare_number']
+	
+	if int(N_gram_number) in chord_list:
+		chord_gram(chordlist,N_gram_number,Compare_number)
+		
 	else :
-		print "請重新輸入N-gram-number，4 or 8 or 16 or 32".decode('cp950')
+		print "請重新選擇chord_pattern，{}".decode('cp950').format(chord_list)
 
 #一次抓4個
-	
-def Dominantlist_4_gram_number(Dominantlist,N_gram_number,Compare_number):
-#     print Dominantlist
-	Dominantgram_4freq = Dominantlist_to_Dominantgram(Dominantlist,N_gram_number)
-#     print Dominantgram_4freq
-	Dominantdict_4freq = Dominantgram_to_Dominantdict(Dominantgram_4freq)
-#     print Dominantdict_4freq
-	Dominantsorted_4freq = sorted(Dominantdict_4freq.items(), key=itemgetter(1), reverse=True)
-#     print Dominantsorted_4freq
+# 一次抓8個
+# 一次抓16個
+# 一次抓32個
+
+def chord_gram(chordlist,N_gram_number,Compare_number):
+#     print lists
+	chordgram_freq = chordlist_to_chordgram(chordlist,N_gram_number)
+#     print chordgram_freq
+	chorddict_freq = chordgram_to_chorddict(chordgram_freq)
+#     print chorddict_freq
+	chordsorted_freq = sorted(chorddict_freq.items(), key=itemgetter(1), reverse=True)
+#     print chordsorted_freq
 #     清除可能的和弦位移
-	Compare_list = Dominantsorted_possible_transfer_clean(Dominantsorted_4freq)
+	Compare_list = chordsorted_possible_transfer_clean(chordsorted_freq)
 #     這裡可以改比對的參數，以第幾個去比對
-	Compare_Dominant,Compare_number = Dominantclean_to_Dominantcompare(Compare_list,Dominantsorted_4freq,Compare_number)
+	Compare_chord,Compare_number = chordclean_to_chordcompare(Compare_list,chordsorted_freq,Compare_number)
 #     開始分類
-	Compare_list3 = Compare_Dominant_classify(Compare_Dominant,Compare_number,N_gram_number)
+	Compare_list3 = Compare_chord_classify(Compare_chord,Compare_number,N_gram_number)
 	
-	Insert_dominant_to_mongodb(Compare_Dominant,N_gram_number)
+	Insert_chord_to_mongodb(Compare_chord,N_gram_number)
 	
-
-#一次抓8個
-
-def Dominantlist_8_gram_number(Dominantlist,N_gram_number,Compare_number):
-#     print Dominantlist
-	Dominantgram_8freq = Dominantlist_to_Dominantgram(Dominantlist,N_gram_number)
-#     print Dominantgram_8freq
-	Dominantdict_8freq = Dominantgram_to_Dominantdict(Dominantgram_8freq)
-#     print Dominantdict_8freq
-	Dominantsorted_8freq = sorted(Dominantdict_8freq.items(), key=itemgetter(1), reverse=True)
-#     print Dominantsorted_8freq
-#     清除可能的和弦位移
-	Compare_list = Dominantsorted_possible_transfer_clean(Dominantsorted_8freq)
-#     這裡可以改比對的參數，以第幾個去比對
-	Compare_Dominant,Compare_number = Dominantclean_to_Dominantcompare(Compare_list,Dominantsorted_8freq,Compare_number)
-#     開始分類
-	Compare_list3 = Compare_Dominant_classify(Compare_Dominant,Compare_number,N_gram_number)
-	
-	Insert_dominant_to_mongodb(Compare_Dominant,N_gram_number)
-
-#一次抓16個
-
-def Dominantlist_16_gram_number(Dominantlist,N_gram_number,Compare_number):
-#     print Dominantlist
-	Dominantgram_16freq = Dominantlist_to_Dominantgram(Dominantlist,N_gram_number)
-#     print Dominantgram_16freq
-	Dominantdict_16freq = Dominantgram_to_Dominantdict(Dominantgram_16freq)
-#     print Dominantdict_16freq
-	Dominantsorted_16freq = sorted(Dominantdict_16freq.items(), key=itemgetter(1), reverse=True)
-#     print Dominantsorted_16freq
-#     清除可能的和弦位移
-	Compare_list = Dominantsorted_possible_transfer_clean(Dominantsorted_16freq)
-#     這裡可以改比對的參數，以第幾個去比對
-	Compare_Dominant,Compare_number = Dominantclean_to_Dominantcompare(Compare_list,Dominantsorted_16freq,Compare_number)
-#     開始分類
-	Compare_list3 = Compare_Dominant_classify(Compare_Dominant,Compare_number,N_gram_number)
-	
-	Insert_dominant_to_mongodb(Compare_Dominant,N_gram_number)
-	
-#一次抓32個
-
-def Dominantlist_32_gram_number(Dominantlist,N_gram_number,Compare_number):
-#     print Dominantlist
-	Dominantgram_32freq = Dominantlist_to_Dominantgram(Dominantlist,N_gram_number)
-#     print Dominantgram_32freq
-	Dominantdict_32freq = Dominantgram_to_Dominantdict(Dominantgram_32freq)
-#     print Dominantdict_32freq
-	Dominantsorted_32freq = sorted(Dominantdict_32freq.items(), key=itemgetter(1), reverse=True)
-#     print Dominantsorted_32freq
-#     清除可能的和弦位移
-	Compare_list = Dominantsorted_possible_transfer_clean(Dominantsorted_32freq)
-#     這裡可以改比對的參數，以第幾個去比對
-	Compare_Dominant,Compare_number = Dominantclean_to_Dominantcompare(Compare_list,Dominantsorted_32freq,Compare_number)
-#     開始分類
-	Compare_list3 = Compare_Dominant_classify(Compare_Dominant,Compare_number,N_gram_number)
-	
-	Insert_dominant_to_mongodb(Compare_Dominant,N_gram_number)
-	
-
 		
 #===============================================================
 #以下是共用def
@@ -223,190 +162,79 @@ def	Insert_dominant_to_mongodb(Compare_Dominant,N_gram_number):
 	
 	# if len(Compare_Dominant) > 3:
 		# for i in range(3):
-			# if N_gram_number == 4:
-				# collect.replace_one({'Dominant_4_gram': Compare_Dominant[i][0]},{'Dominant_4_gram': Compare_Dominant[i][0]},upsert=True)
+			# if int(N_gram_number) in chord_list:
+				# collect.replace_one({str(N_gram_number)+'_gram': Compare_Dominant[i][0]},{str(N_gram_number)+'_gram': Compare_Dominant[i][0]},upsert=True)
 		
-			# elif N_gram_number == 8:
-				# collect.replace_one({'Dominant_8_gram': Compare_Dominant[i][0]},{'Dominant_8_gram': Compare_Dominant[i][0]},upsert=True)
-		
-			# elif N_gram_number == 16:
-				# collect.replace_one({'Dominant_16_gram': Compare_Dominant[i][0]},{'Dominant_16_gram': Compare_Dominant[i][0]},upsert=True)
-		
-			# elif N_gram_number == 32:
-				# collect.replace_one({'Dominant_32_gram': Compare_Dominant[i][0]},{'Dominant_32_gram': Compare_Dominant[i][0]},upsert=True)
 	# else:
 		# for i in range(len(Compare_Dominant)):
-			# if N_gram_number == 4:
-				# collect.replace_one({'Dominant_4_gram': Compare_Dominant[i][0]},{'Dominant_4_gram': Compare_Dominant[i][0]},upsert=True)
-		
-			# elif N_gram_number == 8:
-				# collect.replace_one({'Dominant_8_gram': Compare_Dominant[i][0]},{'Dominant_8_gram': Compare_Dominant[i][0]},upsert=True)
-		
-			# elif N_gram_number == 16:
-				# collect.replace_one({'Dominant_16_gram': Compare_Dominant[i][0]},{'Dominant_16_gram': Compare_Dominant[i][0]},upsert=True)
-		
-			# elif N_gram_number == 32:
-				# collect.replace_one({'Dominant_32_gram': Compare_Dominant[i][0]},{'Dominant_32_gram': Compare_Dominant[i][0]},upsert=True)
+			# if int(N_gram_number) in chord_list:
+				# collect.replace_one({str(N_gram_number)+'_gram': Compare_Dominant[i][0]},{str(N_gram_number)+'_gram': Compare_Dominant[i][0]},upsert=True)
 	
 	
 	collect = db['dominant_gram_pattern_with_no_replace']  #選擇database.collection
 	
+	
 	#搭配 collect = db['dominant_gram_pattern_with_no_replace'] 使用
 	if len(Compare_Dominant) > 3:
 		for i in range(3):
-			if N_gram_number == 4:
-				collect.insert_one({'Dominant_4_gram': Compare_Dominant[i][0]})
+			if int(N_gram_number) in chord_list:
+				collect.insert_one({str(N_gram_number)+'_gram': Compare_Dominant[i][0]})
 		
-			elif N_gram_number == 8:
-				collect.insert_one({'Dominant_8_gram': Compare_Dominant[i][0]})
-		
-			elif N_gram_number == 16:
-				collect.insert_one({'Dominant_16_gram': Compare_Dominant[i][0]})
-		
-			elif N_gram_number == 32:
-				collect.insert_one({'Dominant_32_gram': Compare_Dominant[i][0]})
 	else:
 		for i in range(len(Compare_Dominant)):
-			if N_gram_number == 4:
-				collect.insert_one({'Dominant_4_gram': Compare_Dominant[i][0]})
-		
-			elif N_gram_number == 8:
-				collect.insert_one({'Dominant_8_gram': Compare_Dominant[i][0]})
-		
-			elif N_gram_number == 16:
-				collect.insert_one({'Dominant_16_gram': Compare_Dominant[i][0]})
-		
-			elif N_gram_number == 32:
-				collect.insert_one({'Dominant_32_gram': Compare_Dominant[i][0]})
+			if int(N_gram_number) in chord_list:
+				collect.insert_one({str(N_gram_number)+'_gram': Compare_Dominant[i][0]})
 				
 #從mongodb取出
-def Get_dominant_from_mongodb(N_gram_number):
+def Get_dominant_from_mongodb(**keyword):
 	client = MongoClient('mongodb://10.120.30.8:27017')
 	db = client['music']  #選擇database
-	collect = db['dominant_gram_pattern']  #選擇database.collection
+	collect = db['chord_gram_pattern_with_no_replace']  #選擇database.collection
+	N_gram_number = keyword['N_gram_number']
 	
-	if N_gram_number == 4:
-		cursor = collect.find({'Dominant_4_gram':{'$exists':True}})
-		Dominant_4_gram_list = list()
+	if int(N_gram_number) in chord_list:
+		cursor = collect.find({str(N_gram_number)+'_gram':{'$exists':True}})
+		Gram_list = list()
 		for doc in cursor:
-			Dominant_4_gram_list.append(doc['Dominant_4_gram'])
-		return Dominant_4_gram_list
-	
-	elif N_gram_number == 8:
-		cursor = collect.find({'Dominant_8_gram':{'$exists':True}})
-		Dominant_8_gram_list = list()
-		for doc in cursor:
-			Dominant_8_gram_list.append(doc['Dominant_8_gram'])
-		return Dominant_8_gram_list
-	
-	elif N_gram_number == 16:
-		cursor = collect.find({'Dominant_16_gram':{'$exists':True}})
-		Dominant_16_gram_list = list()
-		for doc in cursor:
-			Dominant_16_gram_list.append(doc['Dominant_16_gram'])
-		return Dominant_16_gram_list
-	
-	elif N_gram_number == 32:
-		cursor = collect.find({'Dominant_32_gram':{'$exists':True}})
-		Dominant_32_gram_list = list()
-		for doc in cursor:
-			Dominant_32_gram_list.append(doc['Dominant_32_gram'])
-		return Dominant_32_gram_list
+			Gram_list.append(doc[str(N_gram_number)+'_gram'])
+		return Gram_list
 		
 	else:
-		print "請重新選擇dominant_pattern，4 or 8 or 16 or 32".decode('cp950')
+		print "請重新選擇chord_pattern，{}".decode('cp950').format(chord_list)
 
 		
-#搭配 collect = db['dominant_gram_pattern_with_no_replace'] 使用
+#搭配 collect = db['chord_gram_pattern_with_no_replace'] 使用
 
-#更新 dominant mongo by group
-def dominant_group_in_mongo(N_gram_number):
+#更新 chord mongo by group
+def chord_group_in_mongo(**keyword):
 	from bson.son import SON
 	from bson.code import Code
 	from pymongo import MongoClient
 
 	client = MongoClient('mongodb://10.120.30.8:27017')
 	db = client['music']  #選擇database
-	collect = db['dominant_gram_pattern_with_no_replace']  #選擇database.collection
-
-	if N_gram_number == 4:
-		pipeline = [
-			 {"$group": {"_id": '$Dominant_4_gram', 
-						 "Dominant_count": {"$sum": 1}}},
-			{"$sort": SON([("Dominant_count", -1), ("_id", 1)])}
-			
-		]
-		Dominant_count_from_mongo = list(collect.aggregate(pipeline))
-
-		for Dominant in Dominant_count_from_mongo:
-			if Dominant['_id'] != None:
-		#         print Dominant['_id'],Dominant['Dominant_count']
-				collect.update_one({"Dominant_4_gram":Dominant['_id']},{'$set':{"Dominant_count": Dominant["Dominant_count"]}})
-
-		# cursor = collect.find({'Dominant_4_gram':{'$exists':True},'Dominant_count':{'$exists':True}})
-		cursor = collect.find({'Dominant_4_gram':{'$exists':True},'Dominant_count':{'$exists':False}})
-
-		for doc in cursor:
-			collect.delete_one(doc)
-			
-	elif N_gram_number == 8:
-		pipeline = [
-			 {"$group": {"_id": '$Dominant_8_gram', 
-						 "Dominant_count": {"$sum": 1}}},
-			{"$sort": SON([("Dominant_count", -1), ("_id", 1)])}
-			
-		]
-		Dominant_count_from_mongo = list(collect.aggregate(pipeline))
-
-		for Dominant in Dominant_count_from_mongo:
-			if Dominant['_id'] != None:
-		#         print Dominant['_id'],Dominant['Dominant_count']
-				collect.update_one({"Dominant_8_gram":Dominant['_id']},{'$set':{"Dominant_count": Dominant["Dominant_count"]}})
-
-		# cursor = collect.find({'Dominant_8_gram':{'$exists':True},'Dominant_count':{'$exists':True}})
-		cursor = collect.find({'Dominant_8_gram':{'$exists':True},'Dominant_count':{'$exists':False}})
-
-		for doc in cursor:
-			collect.delete_one(doc)
-			
-	elif N_gram_number == 16:
-		pipeline = [
-			 {"$group": {"_id": '$Dominant_16_gram', 
-						 "Dominant_count": {"$sum": 1}}},
-			{"$sort": SON([("Dominant_count", -1), ("_id", 1)])}
-			
-		]
-		Dominant_count_from_mongo = list(collect.aggregate(pipeline))
-
-		for Dominant in Dominant_count_from_mongo:
-			if Dominant['_id'] != None:
-		#         print Dominant['_id'],Dominant['Dominant_count']
-				collect.update_one({"Dominant_16_gram":Dominant['_id']},{'$set':{"Dominant_count": Dominant["Dominant_count"]}})
-
-		# cursor = collect.find({'Dominant_16_gram':{'$exists':True},'Dominant_count':{'$exists':True}})
-		cursor = collect.find({'Dominant_16_gram':{'$exists':True},'Dominant_count':{'$exists':False}})
-
-		for doc in cursor:
-			collect.delete_one(doc)
+	collect = db['chord_gram_pattern_with_no_replace']  #選擇database.collection
+	N_gram_number = keyword['N_gram_number']
 	
-	elif N_gram_number == 32:
+	if int(N_gram_number) in chord_list:
 		pipeline = [
-			 {"$group": {"_id": '$Dominant_32_gram', 
-						 "Dominant_count": {"$sum": 1}}},
-			{"$sort": SON([("Dominant_count", -1), ("_id", 1)])}
+			 {"$group": {"_id": '$'+str(N_gram_number)+'_gram', 
+						 "chord_count": {"$sum": 1}}},
+			{"$sort": SON([("chord_count", -1), ("_id", 1)])}
 			
 		]
-		Dominant_count_from_mongo = list(collect.aggregate(pipeline))
+		chord_count_from_mongo = list(collect.aggregate(pipeline))
 
-		for Dominant in Dominant_count_from_mongo:
-			if Dominant['_id'] != None:
-		#         print Dominant['_id'],Dominant['Dominant_count']
-				collect.update_one({"Dominant_32_gram":Dominant['_id']},{'$set':{"Dominant_count": Dominant["Dominant_count"]}})
+		for chord in chord_count_from_mongo:
+			if chord['_id'] != None:
+		#         print chord['_id'],chord['chord_count']
+				collect.update_one({str(N_gram_number)+"_gram":chord['_id']},{'$set':{"chord_count": chord["chord_count"]}})
 
-		# cursor = collect.find({'Dominant_32_gram':{'$exists':True},'Dominant_count':{'$exists':True}})
-		cursor = collect.find({'Dominant_32_gram':{'$exists':True},'Dominant_count':{'$exists':False}})
+		# cursor = collect.find({str(N_gram_number)+"_gram":{'$exists':True},'chord_count':{'$exists':True}})
+		cursor = collect.find({str(N_gram_number)+"_gram":{'$exists':True},'chord_count':{'$exists':False}})
 
 		for doc in cursor:
 			collect.delete_one(doc)
+
 	else:
-		print "請重新選擇dominant_pattern，4 or 8 or 16 or 32".decode('cp950')
+		print "請重新選擇chord_pattern，{}".decode('cp950').format(chord_list)
